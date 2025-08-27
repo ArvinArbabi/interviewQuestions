435207446274e7aa70fc5f30b3936ded684536e5
arcinarbavi <arvin.arbabi50@gmail.com>
2025-08-27 15:04:52 -0700

    adding MissingNumber.java

--- arrays/MissingNumber.java
+++ arrays/MissingNumber.java
@@ -1,9 +1,24 @@
+// Given an array containing n distinct numbers taken from 0, 1, 2, ..., n, 
+// find the single number that is missing.
+
 public class MissingNumber {
     public static int missingNumber(int[] nums) {
-        int n = nums.length, x = n;
-        for (int i = 0; i < n; i++) {
-            x ^= i ^ nums[i];
+        for(int i = 0; i < nums.length-1;i++){
+            if(nums[i+1] != nums[i]+1){
+                return nums[i] + 1;
+            }
         }
-        return x;
+        return -1;
+    }
+
+    //used chat gpt to make test cases
+    public static void main(String[] args) {
+        int[] arr1 = {0, 1, 2, 4, 5}; // missing 3
+        int[] arr2 = {1, 2, 3, 4, 5, 7}; // missing 6
+        int[] arr3 = {0, 1, 2, 3, 4}; // missing 5 (edge case)
+
+        System.out.println("Missing number in arr1: " + missingNumber(arr1));
+        System.out.println("Missing number in arr2: " + missingNumber(arr2));
+        System.out.println("Missing number in arr3: " + missingNumber(arr3));
     }
 }
--- arrays/PivotIndex.java
+++ arrays/PivotIndex.java
@@ -1,12 +1,37 @@
+import java.util.Arrays;
+
 public class PivotIndex {
-    public static int pivotIndex(int[] nums) {
-        long total = 0;
-        for (int x : nums) total += x;
-        long left = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (left == total - left - nums[i]) return i;
-            left += nums[i];
+
+    public static int findPivotIndex(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return -1;
         }
+
+        long totalSum = 0;
+        for (int num : nums) {
+            totalSum += num;
+        }
+
+        long leftSum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (leftSum == totalSum - leftSum - nums[i]) {
+                return i;
+            }
+            leftSum += nums[i];
+        }
+
         return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr1 = {1, 7, 3, 6, 5, 6};
+        System.out.println(findPivotIndex(arr1));
+
+        int[] arr2 = {1, 2, 3};
+        System.out.println(findPivotIndex(arr2));
+
+        int[] arr3 = {2, 1, -1};
+        
+        System.out.println(findPivotIndex(arr3));
     }
 }
