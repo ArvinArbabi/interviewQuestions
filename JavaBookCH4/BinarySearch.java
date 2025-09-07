858f582e9254da3b603e4ffd93166fd202346b5c
arcinarbavi <arvin.arbabi50@gmail.com>
2025-09-07 16:08:50 -0700

    implementing binary search

--- JavaBookCH4/BinarySearch.java
+++ JavaBookCH4/BinarySearch.java
@@ -1,25 +1,28 @@
 public class BinarySearch {
-    public static int searcher(int[] a, int num) {
-        int left = 0, right = a.length;
+    public static int searcher(int[] a, int n) {
+        int left = 0;
+        int right = a.length - 1;
 
-        while (left < right) {
-            int middle = (left + (right-1)) / 2;
+        while (left <= right) {
+            int middle = left + (right - left) / 2;
 
-            if (a[middle] == num) {
-                return middle;
-            } else if (num > a[middle]) {
+            if (n > a[middle]) {
                 left = middle + 1;
-            } else {
+            }
+            else if (n < a[middle]) {
                 right = middle - 1;
             }
+            else {
+                return middle;
+            }
         }
-
         return -1;
     }
+
     public static void main(String[] args) {
-        
-        int[] arr = {1, 3, 5, 7, 9, 11, 13};
+        int[] arr = {1, 3, 5, 7, 9, 11, 13, 15, 19, 20};
         System.out.println(searcher(arr, 7));
-        System.out.println(searcher(arr, 12)); 
+        System.out.println(searcher(arr, 11));
+        System.out.println(searcher(arr, 2)); 
     }
 }
